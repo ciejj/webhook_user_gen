2b3bac3e21be5a0455851555e177fe3dc2c7bed1
ciejj <wozniczka.mateusz@gmail.com>
2024-06-23 23:35:09 +0200

    Add VCR gem and configure test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,26 @@
-ENV["RAILS_ENV"] ||= "test"
-require_relative "../config/environment"
-require "rails/test_help"
+# frozen_string_literal: true
+
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+require 'rails/test_help'
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'test/vcr_cassettes'
+  config.hook_into :webmock # or :fakeweb if you are using fakeweb
+  config.filter_sensitive_data('<PINPOINT_API_KEY>') do
+    ENV['PINPOINT_API_KEY'] || Rails.application.credentials.pinpoint_api.api_key
+  end
+  config.filter_sensitive_data('<HI_BOB_AUTHORIZATION>') do
+    username = ENV['HI_BOB_API_USER'] || Rails.application.credentials.hi_bob_api.user
+    password = ENV['HI_BOB_API_PASSWORD'] || Rails.application.credentials.hi_bob_api.password
+
+    credentials = "#{username}:#{password}"
+    encoded_credentials = Base64.strict_encode64(credentials)
+
+    "Basic #{encoded_credentials}"
+  end
+end
 
 module ActiveSupport
   class TestCase
@@ -9,7 +29,5 @@ module ActiveSupport
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
-
-    # Add more helper methods to be used by all tests here...
   end
 end
